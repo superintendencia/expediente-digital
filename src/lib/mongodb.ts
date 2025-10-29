a15cfa5d03ecc894670b462303b9a55d07cea49a
Superintendencia pjt <superintendenciapjt@gmail.com>
2025-10-29 12:37:10 +0000

    ya esta configurada correctamente la ip

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -17,41 +17,36 @@ if (!MONGODB_DATABASE_NAME) {
 
 // Extend the global type to include our MongoDB connection cache
 declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient>
 }
 
-let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
-    client = new MongoClient(MONGODB_URI);
+    const client = new MongoClient(MONGODB_URI);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(MONGODB_URI);
+  const client = new MongoClient(MONGODB_URI);
   clientPromise = client.connect();
 }
 
 
 let cachedDb: Db | null = null;
 
-async function connectToDatabase() {
+export async function getDb(): Promise<Db> {
     if (cachedDb) {
-        return { db: cachedDb };
+        return cachedDb;
     }
     const client = await clientPromise;
     const db = client.db(MONGODB_DATABASE_NAME);
     cachedDb = db;
-    return { db };
-}
-
-
-export async function getDb(): Promise<Db> {
-    const { db } = await connectToDatabase();
     return db;
 }
